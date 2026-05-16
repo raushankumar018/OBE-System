3f49a9a82f752568047799794e90061f12660125
cs-satyam <satyamwork4464@gmail.com>
2026-05-16 09:26:09 +0530

    fix model  session

--- server/models/User.js
+++ server/models/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   picture: { type: String },
   role: { type: String, enum: ['faculty', 'admin', 'hod'], default: 'faculty' },
   department: { type: String, default: 'Computer Science and Engineering' },
-  university: { type: String, default: "Vignan's University" },
+  university: { type: String, default : "Vignan's University" },
   sessions: [{ type: String }], // session_ids from Python API
   createdAt: { type: Date, default: Date.now },
   lastLogin: { type: Date, default: Date.now },
